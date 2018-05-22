9642cb56cd2736436883a91ad800efe6efd00b49
pipdeveloper <pipdevs@gmail.com>
2018-05-22 16:53:05 +0000

    project build by Travis CI [skip ci]

--- obj/src/container/AnnouncementsProcess.js
+++ obj/src/container/AnnouncementsProcess.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
+const pip_services_net_node_1 = require("pip-services-net-node");
+const pip_services_oss_node_1 = require("pip-services-oss-node");
 const AnnouncementsServiceFactory_1 = require("../build/AnnouncementsServiceFactory");
 class AnnouncementsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("announcements", "System announcements microservice");
         this._factories.add(new AnnouncementsServiceFactory_1.AnnouncementsServiceFactory);
+        this._factories.add(new pip_services_net_node_1.DefaultNetFactory);
+        this._factories.add(new pip_services_oss_node_1.DefaultOssFactory);
     }
 }
 exports.AnnouncementsProcess = AnnouncementsProcess;
