8bc4483fe0e880d0c96fc19e108efe7658073a09
pipdeveloper <pipdevs@gmail.com>
2019-04-25 09:54:08 +0000

    project build by Travis CI [skip ci]

--- obj/src/logic/AnnouncementsCommandSet.d.ts
+++ obj/src/logic/AnnouncementsCommandSet.d.ts
@@ -3,10 +3,10 @@ import { IAnnouncementsController } from './IAnnouncementsController';
 export declare class AnnouncementsCommandSet extends CommandSet {
     private _logic;
     constructor(logic: IAnnouncementsController);
-    private makeGetAnnouncementsCommand;
-    private makeGetRandomAnnouncementCommand;
-    private makeGetAnnouncementByIdCommand;
-    private makeCreateAnnouncementCommand;
-    private makeUpdateAnnouncementCommand;
-    private makeDeleteAnnouncementByIdCommand;
+    private makeGetAnnouncementsCommand();
+    private makeGetRandomAnnouncementCommand();
+    private makeGetAnnouncementByIdCommand();
+    private makeCreateAnnouncementCommand();
+    private makeUpdateAnnouncementCommand();
+    private makeDeleteAnnouncementByIdCommand();
 }
